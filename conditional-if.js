865c4e0a1ccffa4de4ce231525965d70c21bbfeb
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-07-16 19:14:18 -0700

    functions and toLowerCase

--- conditional-if.js
+++ conditional-if.js
@@ -1,16 +1,34 @@
 
-
-function playGuessingGame() {
+function promptUser(){
     let userChoice;
-    let correctAnswer;
-    correctAnswer = "cat";
+    let lowerCaseChoice;
     alert("guess the animal I am thinking of");
     userChoice = prompt("Enter an animal. HINT: it is a fur ball.");
+    lowerCaseChoice = userChoice.toLowerCase();
+    return lowerCaseChoice;
+}
 
-    if( correctAnswer === userChoice ) {
-        alert("You win!");
+function win() {
+    alert("You win!");
+    window.location.href = "https://www.pexels.com/photo/white-and-grey-kitten-on-brown-and-black-leopard-print-textile-45201/";
+}
+
+function lose() {
+    alert("Try again!");
+}
+
+
+function playGuessingGame() {
+    let guess;
+    let correctAnswer;
+    correctAnswer = "cat";
+
+    guess = promptUser();
+
+    if( correctAnswer === guess ) {
+        win();
     } else {
-        alert("Try again!")
+        lose();
     }
 }
 
