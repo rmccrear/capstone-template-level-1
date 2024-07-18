f7365ab628fd3158291b1c8c69dca0fa0c57b9a7
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-07-17 23:39:22 -0700

    update for-loops with more ex

--- push-on-array.js
+++ push-on-array.js
@@ -1,3 +1,13 @@
+function randomIndex(len) {
+    // get a random number between 0 and 1.
+    let rand1 = Math.random();
+    // scale the number up to the length of the array
+    let rand2 = rand1 * len;
+    // convert to whole number
+    let index = Math.floor(rand2);
+    return index;
+}
+
 
 let cuteAnimals = ["Cat", "Dog", "Bunny", "Hamster"];
 let scaryAnimals = ["lion", "tiger", "bear"];
@@ -7,8 +17,8 @@ console.log(scaryAnimals[1])
 console.log(scaryAnimals[2])
 console.log(scaryAnimals.length)
 
-function doJob() {
-    for(let i=0; i<scaryAnimals.length; i++) {
+function combineArrays() {
+    for (let i = 0; i < scaryAnimals.length; i++) {
         let a;
         a = scaryAnimals[i];
         cuteAnimals.push(a);
@@ -16,5 +26,32 @@ function doJob() {
     console.log(cuteAnimals);
 }
 
+pushArray.onclick = combineArrays;
 
-pushArray.onclick = doJob;
+
+function showRandom() {
+    let animals = ["Cat", "Dog", "Bunny", "Hamster"];
+    let randomNumber = randomIndex(animals.length);
+    let a = animals[randomNumber];
+    alert(`Your random animals is ${a}`);
+}
+
+randomButton.onclick = showRandom;
+
+
+function createList(){
+    for(let i=0; i<cuteAnimals.length; i++) {
+        animalList.innerHTML += `<li>${cuteAnimals[i]}</li>`;
+    }
+
+    // let i = 0;
+    // animalList.innerHTML += `<li>${cuteAnimals[i]}</li>`;
+    // i++
+    // animalList.innerHTML += `<li>${cuteAnimals[i]}</li>`;
+    // i++
+    // animalList.innerHTML += `<li>${cuteAnimals[i]}</li>`;
+    // i++
+    // animalList.innerHTML += `<li>${cuteAnimals[i]}</li>`;
+}
+
+createListButton.onclick = createList;
